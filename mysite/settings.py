b7afb318450c79393cb3f8140f97fe86d70c8297
neeraj3030 <neeraj.panwar2016@gmail.com>
2020-03-15 17:17:09 +0530

    Added whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -179,8 +179,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"media_cdn")
 
 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 django_heroku.settings(locals())
