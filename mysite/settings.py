0cddf89bf866890aa81912b4475745fd87d70adc
Neeraj Panwar <49247372+npneeraj@users.noreply.github.com>
2020-03-26 00:03:07 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '6!b6nw@f_+zdh@t+27(b35u609&6zead4a2kcb*0vwdo*61@ng'
-SECRET_KEY= os.environ.get('SECRET_KEY')
+SECRET_KEY = '6!b6nw@f_+zdh@t+27(b35u609&6zead4a2kcb*0vwdo*61@ng'
+#SECRET_KEY= os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG=(os.environ.get('DEBUG_VALUE')=='True')
+DEBUG = True
+#DEBUG=(os.environ.get('DEBUG_VALUE')=='True')
 
 ALLOWED_HOSTS = ['eddaemon.herokuapp.com']
 #for sending email
